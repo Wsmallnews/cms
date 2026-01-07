87d891bc3ac27c0f9216cc814ac339cf8c73c90b
smallnews@qq.com <1371606921@qq.com>
2026-01-07 18:48:44 +0800

    双因素认证

--- resources/views/livewire/tradition/settings/two-factor.blade.php
+++ resources/views/livewire/tradition/settings/two-factor.blade.php
@@ -1,9 +1,11 @@
 <div class="w-full flex flex-col grow">
-    <livewire:sn-cms-components-navigation scope-type="{{ $this->getScopeType() }}" scope-id="{{ $this->getScopeId() }}" />
+    {{-- <livewire:sn-cms-components-navigation scope-type="{{ $this->getScopeType() }}" scope-id="{{ $this->getScopeId() }}" /> --}}
 
-    <div class="w-full flex flex-col grow gap-4">
-        <livewire:sn-user-components-settings-two-factor :module="app(\Wsmallnews\Cms\CmsPlugin::class)->getId()" />
+    <div class="container mx-auto flex flex-col grow gap-4">
+        <div class="w-full mx-auto md:w-96 p-4">
+            <livewire:sn-user-components-settings-two-factor :module="app(\Wsmallnews\Cms\CmsPlugin::class)->getId()" />
+        </div>
     </div>
 
-    <livewire:sn-cms-components-footer scope-type="{{ $this->getScopeType() }}" scope-id="{{ $this->getScopeId() }}" />
+    {{-- <livewire:sn-cms-components-footer scope-type="{{ $this->getScopeType() }}" scope-id="{{ $this->getScopeId() }}" /> --}}
 </div>
