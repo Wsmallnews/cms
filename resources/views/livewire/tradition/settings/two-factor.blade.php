1353b0d2616ad58a3aa85fcef8d379946b1ce61f
smallnews <1371606921@qq.com>
2026-01-08 23:42:26 +0800

    引入 support css

--- resources/views/livewire/tradition/settings/two-factor.blade.php
+++ resources/views/livewire/tradition/settings/two-factor.blade.php
@@ -2,7 +2,7 @@
     {{-- <livewire:sn-cms-components-navigation scope-type="{{ $this->getScopeType() }}" scope-id="{{ $this->getScopeId() }}" /> --}}
 
     <div class="container mx-auto flex flex-col grow gap-4">
-        <div class="w-full mx-auto md:w-96 p-4">
+        <div class="w-full mx-auto p-4">
             <livewire:sn-user-components-settings-two-factor :module="app(\Wsmallnews\Cms\CmsPlugin::class)->getId()" />
         </div>
     </div>
