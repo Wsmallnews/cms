41c8dc0dda674c40287cabed283f7693de5a74f8
smallnews@qq.com <1371606921@qq.com>
2026-01-20 15:30:27 +0800

    个人中心菜单增加 种质申请

--- src/Livewire/Components/User/ProfileMenu.php
+++ src/Livewire/Components/User/ProfileMenu.php
@@ -15,6 +15,10 @@ class ProfileMenu extends Base
                 'url' => Utils::route('profile'),
             ],
             [
+                'label' => '种质申请',
+                'url' => Utils::route('user.appraise-applies'),
+            ],
+            [
                 'label' => '修改资料',
                 'url' => Utils::route('settings.profile'),
             ],
@@ -22,11 +26,14 @@ class ProfileMenu extends Base
                 'label' => '修改密码',
                 'url' => Utils::route('settings.password'),
             ],
-            [
+        ];
+
+        if (Utils::getConfig('two-factor.enabled', false)) {
+            $sidebar[] = [
                 'label' => '双因素认证',
                 'url' => Utils::route('settings.two-factor'),
-            ],
-        ];
+            ];
+        }
 
         return view($this->getThemeView('components.user.profile-menu'), [
             'sidebar' => $sidebar,
