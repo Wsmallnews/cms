b6e98c0ec831367f75483bf4fee0786d67f5171d
smallnews <1371606921@qq.com>
2026-05-31 00:49:47 +0800

    隐藏 page 页面 heading，使用组件的 heading

--- src/Filament/Pages/Navigation/Base.php
+++ src/Filament/Pages/Navigation/Base.php
@@ -68,6 +68,11 @@ abstract class Base extends Page
         return static::$title ?? __('sn-cms::cms.navigation_page.title');
     }
 
+    public function getHeading(): string | Htmlable | null
+    {
+        return null;
+    }
+
     public static function getNavigationLabel(): string
     {
         return static::$navigationLabel ?? static::$title ?? __('sn-cms::cms.navigation_page.navigation_label');
